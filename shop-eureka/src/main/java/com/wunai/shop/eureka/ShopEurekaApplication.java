4f535393f5a78fc52274aaefd049786e954564b0
wanglebo <wanglebo@zaonline.com>
2019-04-29 14:49:18 +0800

    注册中心端口配置

--- shop-eureka/src/main/java/com/wunai/shop/eureka/ShopEurekaApplication.java
+++ shop-eureka/src/main/java/com/wunai/shop/eureka/ShopEurekaApplication.java
@@ -1,9 +1,9 @@
-package com.wunai.shop.shopeureka;
+package com.wunai.shop.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.wunai.shop" })
 public class ShopEurekaApplication {
 
 	public static void main(String[] args) {
