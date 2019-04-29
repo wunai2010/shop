a391d2f04f7124b148db77aa1977cfbfe2597fd1
wunai2010 <wanglebai2008@163.com>
2019-04-29 20:23:47 +0800

    包名修改

--- shop-user/src/main/java/com/wunai/shop/user/ShopUserApplication.java
+++ shop-user/src/main/java/com/wunai/shop/user/ShopUserApplication.java
@@ -1,4 +1,4 @@
-package com.wunai.shop.user.shopuser;
+package com.wunai.shop.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
